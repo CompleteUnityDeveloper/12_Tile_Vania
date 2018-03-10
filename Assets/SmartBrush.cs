537eb02a10723e37151decda99c35b1b008396ea
Ben Tristem <github@bentristem.com>
2018-03-10 23:56:10 +0000

    SmartBrush selectable and Paint() called

---
+++ Assets/SmartBrush.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEditor; // so we can inherit from GridBrush 
+
+[CustomGridBrush()] // required for brush to show at bottom-left of TilePalette
+public class SmartBrush : GridBrush
+{
+    // Note manual has wrong parameter name grid instead of gridLayout!
+    public override void Paint(GridLayout gridLayout, GameObject brushTarget, Vector3Int position)
+    {
+        Debug.Log("SmartBrush Paint called");
+    }
+}
