92e009947af0a1b84b7dca0a7e7a2e6624abeb42
Ben Tristem <github@bentristem.com>
2018-03-11 00:15:50 +0000

    Random surface brush working :-)

--- Assets/SmartBrush.cs
+++ Assets/SmartBrush.cs
@@ -4,12 +4,34 @@ using UnityEngine;
 using UnityEngine.Tilemaps;
 using UnityEditor; // so we can inherit from GridBrush 
 
-[CustomGridBrush()] // required for brush to show at bottom-left of TilePalette
+// required for brush to show at bottom-left of TilePalette
+// parameters restrict to just the SO brush
+[CustomGridBrush(false, true, false, "Smart Brush")]
 public class SmartBrush : GridBrush
 {
+    [SerializeField] TileBase[] randomTiles; // on Scriptable Object
+
+    [MenuItem("Assets/Create/Brushes/SmartBrush")]
+    public static void CreateBrush()
+    {
+        string path = EditorUtility.SaveFilePanelInProject(
+            "Save Smart Brush",
+            "New Smart Brush",
+            "asset",
+            "Save Smart Brush",
+            "Assets/Brushes"
+        ); // todo clarify why this list
+        AssetDatabase.CreateAsset(CreateInstance<SmartBrush>(), path);
+    }
+
     // Note manual has wrong parameter name grid instead of gridLayout!
     public override void Paint(GridLayout gridLayout, GameObject brushTarget, Vector3Int position)
     {
-        Debug.Log("SmartBrush Paint called");
+        Debug.Log("Paint() called at " + position);
+        var randomTile = randomTiles[(int)(randomTiles.Length * Random.value)];
+        var tilemap = brushTarget.GetComponent<Tilemap>();
+        tilemap.SetTile(position, randomTile);
     }
+
+
 }
