b9c31246622f75b89473403a31ff9adaedb40620
Ben Tristem <github@bentristem.com>
2018-03-28 22:42:45 +0100

    Water rises

---
+++ Assets/Scripts/Flood.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flood : MonoBehaviour {
+
+    [SerializeField] float floodRate = 0.2f;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float yMove = floodRate * Time.deltaTime;
+        transform.Translate(new Vector2(0f, yMove));
+	}
+}
