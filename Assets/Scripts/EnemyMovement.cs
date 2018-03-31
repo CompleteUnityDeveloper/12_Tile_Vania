2baa5e3059674caaecc94cd44d6eb094655e3c74
Rick <rickinaction@gmail.com>
2018-03-31 17:17:42 +1000

    Enemy moves on platform, flips, turns around when gets to end.

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -4,18 +4,41 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour {
 
-    [SerializeField] float moveSpeed = .5f;
+    [SerializeField] float moveSpeed = .01f;
+
+    bool facingRight = false;
 
     Rigidbody2D myRigidBody;
     
-    // Use this for initialization
+
     void Start () {
         myRigidBody = GetComponent<Rigidbody2D>();
-        
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+
+    void Update () {
+        MoveHorizontally();        
+    }
+
+    private void MoveHorizontally()
+    {
+        if (facingRight == true)
+        {
+            Vector2 enemyVelocity = new Vector2(moveSpeed, 0f);
+            myRigidBody.velocity = enemyVelocity;
+        }
+        else
+        {
+            Vector2 enemyVelocity = new Vector2(-moveSpeed, 0f);
+            myRigidBody.velocity = enemyVelocity;
+        }
+    } 
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        print("No longer in contact with " + other.transform.name);
+        facingRight = !facingRight;
+        transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x), 1f);
+    }
+
 }
