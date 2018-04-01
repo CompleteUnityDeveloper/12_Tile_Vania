bc0efe5e40193c99247060574b5c86485d036c2d
Rick <rickinaction@gmail.com>
2018-04-01 11:46:46 +1000

    Added some random colour variation in enemies for fun.

---
+++ Assets/EnemyColour.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyColour : MonoBehaviour {
+
+    SpriteRenderer spriteRenderer;
+
+	// Use this for initialization
+	void Start () {
+        Color newColor = new Color(Random.Range(0f, 1f), Random.Range(0.5f,1f), Random.Range(0.5f, 1f));
+        GetComponent<SpriteRenderer>().color = newColor;
+	}	
+}
