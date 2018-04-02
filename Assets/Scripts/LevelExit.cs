7f5459cd3b1ea583c43501b88a7a83e34daff502
Rick <rickinaction@gmail.com>
2018-04-02 16:01:12 +1000

    Load next level mechanism working

---
+++ Assets/Scripts/LevelExit.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Level Exit")
+        {
+            StartCoroutine(LoadNextLevel());
+        }
+    }
+
+    IEnumerator LoadNextLevel()
+    {
+        Time.timeScale = 0.1f;
+        yield return new WaitForSecondsRealtime(2);
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.sceneCount - 1);
+    }
+}
