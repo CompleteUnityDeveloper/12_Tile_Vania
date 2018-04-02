624c12d46065db3d685a2f4836208fda01d8509a
Rick <rickinaction@gmail.com>
2018-04-02 12:25:41 +1000

    Added pause menu, starting to build main menu.

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class PauseMenu : MonoBehaviour {
+
+    public static bool GameIsPaused = false;
+    [SerializeField] GameObject pauseMenuUI;
+    [SerializeField] Scene mainMenu;
+
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.GetKeyDown(KeyCode.P))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+	}
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(0);
+        Resume();
+    }
+
+    public void QuitToMainMenu()
+    {
+        print("quitting game");
+    }
+ 
+}
