431e28b07c993674b808313bed5e9582dc03172a
Rick <rickinaction@gmail.com>
2018-04-04 10:13:23 +1000

    Music player persists when player dies - Singleton pattern implemented

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+    void Awake()
+    {
+        int numMusicPlayers = FindObjectsOfType<MusicPlayer>().Length;
+        if (numMusicPlayers > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
