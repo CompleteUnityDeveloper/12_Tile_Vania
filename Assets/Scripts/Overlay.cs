e93cf73107967b09a0a0c92da53b507d9ef2ca7a
robFraser1111 <robfraserdesign@gmail.com>
2021-08-16 21:13:58 +1000

    Map transition effects.

---
+++ Assets/Scripts/Overlay.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Overlay : MonoBehaviour
+{
+
+    private float alpha = 0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Start off with the canvas being transparent
+        gameObject.GetComponent<Image>().color = new UnityEngine.Color(0f, 0f, 0f, alpha);
+    }
+
+    public void FadeOut(float time)
+    {
+        // Interpolate from transparent to dark over a set period of time
+        alpha = Mathf.Lerp(0f, 1f, time);
+        gameObject.GetComponent<Image>().color = new UnityEngine.Color(0f, 0f, 0f, alpha);
+    }
+
+}
---
+++ Assets/Scripts/Portals.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Portals : MonoBehaviour
+{
+
+    [SerializeField] AudioClip loadLevelSFX;
+    private bool portal = false;
+
+    void Update()
+    {
+        PortalKey();
+    }
+
+
+    private void PortalKey()
+    {
+        if (CrossPlatformInputManager.GetButtonDown("Jump") && portal == true)
+        {
+            Debug.Log("Loaded level " + gameObject.name);
+            StartCoroutine("LoadLevel");
+        }
+    }
+
+    IEnumerator LoadLevel()
+    {
+        FindObjectOfType<Overlay>().FadeOut(2f);
+
+        AudioSource.PlayClipAtPoint(loadLevelSFX, transform.position);
+
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene(gameObject.name);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            portal = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            portal = false;
+        }
+    }
+
+}
